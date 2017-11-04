46f1e06aec141e7da2b8f540dfab318538324d57
Akuma Majunia <akuma.majunia@gmail.com>
2017-11-04 08:23:20 -0200

    Comentarios

--- algoritmos/cpp/staircase.cpp
+++ algoritmos/cpp/staircase.cpp
@@ -6,6 +6,42 @@
 
 using namespace std;
 
+/*
+Consider a staircase of size :
+
+   #
+  ##
+ ###
+####
+Observe that its base and height are both equal to , and the image is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+Write a program that prints a staircase of size .
+
+Input Format
+
+A single integer, , denoting the size of the staircase.
+
+Output Format
+
+Print a staircase of size  using # symbols and spaces.
+
+Note: The last line must have  spaces in it.
+
+Sample Input
+
+6 
+Sample Output
+
+     #
+    ##
+   ###
+  ####
+ #####
+######
+Explanation
+
+The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of .
+*/
 int main(){
     int nr_tamanho;
     cin >> nr_tamanho;
