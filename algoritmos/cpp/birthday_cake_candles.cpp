4fdd8dfb56913206aafc541f871c02e3bda6d0de
Akuma Majunia <akuma.majunia@gmail.com>
2017-11-04 13:47:25 -0200

    birthday cake candles resolvido cpp

--- algoritmos/cpp/birthday_cake_candles.cpp
+++ algoritmos/cpp/birthday_cake_candles.cpp
@@ -1,3 +1,34 @@
+/**
+Colleen is turning  years old! Therefore, she has  candles of various heights on her cake, and candle  has height . Because the taller candles tower over the shorter ones, Colleen can only blow out the tallest candles.
+
+Given the  for each individual candle, find and print the number of candles she can successfully blow out.
+
+Input Format
+
+The first line contains a single integer, , denoting the number of candles on the cake. 
+The second line contains  space-separated integers, where each integer  describes the height of candle .
+
+Constraints
+
+Output Format
+
+Print the number of candles Colleen blows out on a new line.
+
+Sample Input 0
+
+4
+3 2 1 3
+Sample Output 0
+
+2
+Explanation 0
+
+We have one candle of height , one candle of height , and two candles of height . Colleen only blows out the tallest candles, meaning the candles where . Because there are  such candles, we print  on a new line.
+
+Portugues:
+    devemos encontrar  a mior vela
+    depois devemos contar qntas delas tem e retornar
+*/
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -6,8 +37,33 @@
 
 using namespace std;
 
-int main(){
-    
+int birthdayCakeCandles(int nr_valores, vector <int> arrValores) {
+    int nr_maior_valor = 0;
+    int nr_contador = 0;
 
+    // descobrir a maior vela
+    for(int i=0; i<nr_valores; i++) {
+        nr_maior_valor = (arrValores[i] > nr_maior_valor ? arrValores[i] : nr_maior_valor);
+    }
+
+    // conta a maior
+    for(int i=0; i<nr_valores; i++) {
+        nr_contador += (arrValores[i] == nr_maior_valor ? 1 : 0);
+    }
+
+    return nr_contador;
+}
+
+int main() {
+    int nr_valores;
+    cin >> nr_valores;
+    vector<int> arrValores(nr_valores);
+
+    for(int arrValores_i = 0; arrValores_i < nr_valores; arrValores_i++){
+       cin >> arrValores[arrValores_i];
+    }
+
+    int nr_resultado = birthdayCakeCandles(nr_valores, arrValores);
+    cout << nr_resultado << endl;
     return 0;
 }
---
+++ algoritmos/cpp/dicas_cpp.cpp
@@ -0,0 +1,20 @@
+/**
+Este arquivo contem dicas loucas de C++
+*/
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	// o operador !! converte qualquer valor para zero ou 1
+	cout << !!(3) << endl;
+	cout << !!(2) << endl;
+	cout << !!(1) << endl;
+	cout << !!(0) << endl;
+
+	return 0;
+}
